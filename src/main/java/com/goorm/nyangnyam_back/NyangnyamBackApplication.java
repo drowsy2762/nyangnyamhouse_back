d170c7a8d04fefecad05eb2cc5036363bbc39d22
drowsy2762 <drowsy2762@gmail.com>
2024-06-24 23:31:06 +0900

    ? 어디에 연결된건데

---
+++ src/main/java/com/goorm/nyangnyam_back/NyangnyamBackApplication.java
@@ -0,0 +1,13 @@
+package com.goorm.nyangnyam_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyangnyamBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyangnyamBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goorm/nyangnyam_back/NyangnyamBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goorm.nyangnyam_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyangnyamBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
