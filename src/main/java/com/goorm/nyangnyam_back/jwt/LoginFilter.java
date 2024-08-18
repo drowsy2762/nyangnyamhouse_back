b01677c29f3768fb7c24ad0c49dd74bd88bd131a
jin2304 <search2304@naver.com>
2024-08-19 00:06:20 +0900

    feat: LoginFilter에 successfulAuthentication() 및 unsuccessfulAuthentication() 추가
    -successfulAuthentication(): 로그인 성공 시 실행하는 메소드
    -unsuccessfulAuthentication(): 로그인 실패 시 실행하는 메소드

--- src/main/java/com/goorm/nyangnyam_back/jwt/LoginFilter.java
+++ src/main/java/com/goorm/nyangnyam_back/jwt/LoginFilter.java
@@ -10,10 +10,12 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.util.StreamUtils;
@@ -129,5 +131,26 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
         } catch (IOException e) {
             throw new RuntimeException("Failed to read JSON data", e);
         }
+    }
+
+
+    /**
+     * 로그인 성공 시 실행하는 메소드
+     */
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
+        System.out.println("로그인 성공");
+        response.setStatus(HttpStatus.OK.value());
+    }
+
+
+    /**
+     * 로그인 실패 시 실행하는 메소드
+     */
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException {
+        System.out.println("로그인 실패: " + failed.getMessage());
+        response.getWriter().write("Authentication failed: " + failed.getMessage());
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
     }
 }
