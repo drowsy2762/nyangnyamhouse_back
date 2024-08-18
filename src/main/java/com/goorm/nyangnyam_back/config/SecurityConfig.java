54859bb9fcf23bcd03f2f3bb5d4fce810e0c1834
jin2304 <search2304@naver.com>
2024-08-18 22:07:51 +0900

    feat(security): SecurityFilterChain에 CustomAuthenticationProvider 및 LoginFilter 등록

--- src/main/java/com/goorm/nyangnyam_back/config/SecurityConfig.java
+++ src/main/java/com/goorm/nyangnyam_back/config/SecurityConfig.java
@@ -1,15 +1,42 @@
 package com.goorm.nyangnyam_back.config;
 
+import com.goorm.nyangnyam_back.jwt.CustomAuthenticationProvider;
+import com.goorm.nyangnyam_back.jwt.LoginFilter;
+import com.goorm.nyangnyam_back.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final UserRepository userRepository;
+    private final CustomAuthenticationProvider customAuthenticationProvider;
+
+    @Autowired
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, UserRepository userRepository, CustomAuthenticationProvider customAuthenticationProvide) {
+        this.authenticationConfiguration = authenticationConfiguration;
+        this.userRepository = userRepository;
+        this.customAuthenticationProvider = customAuthenticationProvide;
+    }
+
+
+    //AuthenticationManager 등록
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -37,6 +64,17 @@ public class SecurityConfig {
         http
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+
+        //CustomAuthenticationProvider 등록
+        http
+                .authenticationProvider(customAuthenticationProvider);
+
+
+        //LoginFilter 등록
+        http
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), userRepository), UsernamePasswordAuthenticationFilter.class);
+
 
         return http.build();
     }
