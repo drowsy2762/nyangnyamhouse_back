91b78dcb7400d72abf4de4ac3445857773978c29
drowsy2762 <drowsy2762@gmail.com>
2024-08-29 17:31:57 +0900

    db 연결시도 그러나 그럼에도 오류발생

--- src/main/java/com/goorm/nyangnyam_back/NyangnyamBackApplication.java
+++ src/main/java/com/goorm/nyangnyam_back/NyangnyamBackApplication.java
@@ -2,8 +2,10 @@ package com.goorm.nyangnyam_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.goorm.nyangnyam_back.repository")
 public class NyangnyamBackApplication {
     public static void main(String[] args) {
         SpringApplication.run(NyangnyamBackApplication.class, args);
