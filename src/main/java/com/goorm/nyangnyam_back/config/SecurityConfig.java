a110fea1595d40e582fd69a3504b47d7359441e8
drowsy2762 <drowsy2762@gmail.com>
2025-01-09 17:49:59 +0900

    프로파일 dev 업데이트

--- src/main/java/com/goorm/nyangnyam_back/config/SecurityConfig.java
+++ src/main/java/com/goorm/nyangnyam_back/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.goorm.nyangnyam_back.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,6 +17,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @Configuration
+@Profile("!dev")
 @EnableWebSecurity
 public class SecurityConfig {
 
